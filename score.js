c586275c2f884eb7599d8994474a00a84fa1a60e
Akash9698 <senthilsenthil98832@gmail.com>
2024-03-01 10:16:31 +0530

    done

--- score.js
+++ score.js
@@ -1,1 +1,8 @@
 // Iteration 8: Making scoreboard functional
+
+const scoreboard=document.getElementById("score-board")
+scoreboard.innerText=localStorage.getItem("total")
+
+const playagain=document.getElementById("play-again-button").addEventListener('click',()=>{
+    location.href='game.html'
+})
